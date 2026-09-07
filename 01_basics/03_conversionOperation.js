fb69963afaf5e60fc97d67b48d151ce09d1d11a2
mohitthakkar1105 <mohit.thakkar1105@gmail.com>
2026-09-07 16:19:55 +0530

    type conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,3 +29,38 @@ let someValue = true;
 let stringValue = String(someValue);
 console.log(stringValue, typeof stringValue); 
 // 33 -> "33" , null -> "null" , undefined -> "undefined" , true -> "true" , false -> "false"
+
+
+// ****************************operation****************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + (2 + 2)); // 14
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
