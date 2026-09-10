f192da73e6eb62f1b38b83a03a2df449478f51ea
mohitthakkar1105 <mohit.thakkar1105@gmail.com>
2026-09-10 20:03:53 +0530

    loops

---
+++ 05_loops/01_loops.js
@@ -0,0 +1,18 @@
+const array = [1,2,3,4,5,6]
+
+// for loop
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+}
+// break , continue 
+
+// while
+while (condition) {
+    
+}
+
+// do while - > isme atleast ek bar to chalega 
+do {
+    
+} while (condition);
